b982f072bda42cb155ae157cc7f38b3d734d2605
vtrpaulop <vtrpaulop@gmail.com>
2024-10-19 19:48:58 -0300

    Criando PageAdmin e TPL admin

--- vendor/hcodebr/php-classes/src/Page.php
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -11,14 +11,14 @@ class Page
     private $options = [];
     private $default = ["data" => []];
 
-    public function __construct($opts = array())
+    public function __construct($opts = array(), $tpl_dir = "/views/")
     {
 
         $this->options = array_merge($this->default, $opts);
 
         // config
         $config = array(
-            "tpl_dir" => $_SERVER["DOCUMENT_ROOT"] . "/views/",
+            "tpl_dir" => $_SERVER["DOCUMENT_ROOT"] . $tpl_dir,
             "cache_dir" => $_SERVER["DOCUMENT_ROOT"] . "/views-cache/",
             "debug" => false // set to false to improve the speed
         );
